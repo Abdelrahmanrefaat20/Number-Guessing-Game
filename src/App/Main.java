270ec553e19338d9f7952c071dce32dad77efbda
jinx <abdelrahman@mail.de>
2026-04-04 19:44:28 +0200

    Updated

--- src/App/Main.java
+++ src/App/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Validator validator = new Validator();
         while (true) {
+            boolean won = false;
             Game game = new Game(1, 100);
             System.out.println("Welcome! Guess a number between 1 and 100. You have 10 attempts.");
             while (!game.isGameOver()) {
@@ -32,11 +33,12 @@ public class Main {
 
                 if (result.equals("Correct")) {
                     System.out.println("Correct! You guessed it in " + game.getAttempts() + " attempts.");
+                    won = true;
                     break;
                 }
                 System.out.println(result + " " + game.getRemaining() + " attempts remaining.");
             }
-            if (game.getRemaining() == 0) {
+            if (game.getRemaining() == 0 || !won ) {
                 System.out.println("You lose. The number was: " + game.getNumber());
             }
             System.out.print("Play again? (yes/no): ");
