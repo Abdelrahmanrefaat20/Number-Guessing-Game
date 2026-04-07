99f2632a23aab0c229b2d42282cbfe03e6738e7a
jinx <abdelrahman@mail.de>
2026-04-07 16:19:12 +0200

    Update Gui

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,6 +1,8 @@
 package App;
 import java.util.Scanner;
 import Game.*;
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,39 +11,37 @@ public class Main {
         while (true) {
             boolean won = false;
             Game game = new Game(1, 100);
-            System.out.println("Welcome! Guess a number between 1 and 100. You have 10 attempts.");
+            JOptionPane.showMessageDialog(null,"Welcome! Guess a number between 1 and 100. You have 10 attempts.");
             while (!game.isGameOver()) {
                 int attemptNum = game.getAttempts() + 1;
-                System.out.print("Attempt " + attemptNum + "/" + game.getMaxAttempts() + " — Enter your guess: ");
 
-                String input = sc.next();
+                String input =  JOptionPane.showInputDialog("Attempt " + attemptNum + "/" + game.getMaxAttempts() + " — Enter your guess: ");
 
                 if (!validator.isValidNumber(input)) {
-                    System.out.println("Invalid input");
+                    JOptionPane.showMessageDialog(null,"Invalid input");
                     continue;
                 }
 
                 int guess = Integer.parseInt(input);
 
                 if (!validator.inRange(1, 100, guess)) {
-                    System.out.println("Out of range");
+                    JOptionPane.showMessageDialog(null,"Out of range");
                     continue;
                 }
 
                 String result = game.checkGuess(guess);
 
                 if (result.equals("Correct")) {
-                    System.out.println("Correct! You guessed it in " + game.getAttempts() + " attempts.");
+                    JOptionPane.showMessageDialog(null,"Correct! You guessed it in " + game.getAttempts() + " attempts.");
                     won = true;
                     break;
                 }
-                System.out.println(result + " " + game.getRemaining() + " attempts remaining.");
+                JOptionPane.showMessageDialog(null,result + " " + game.getRemaining() + " attempts remaining.");
             }
-            if (game.getRemaining() == 0 || !won ) {
-                System.out.println("You lose. The number was: " + game.getNumber());
+            if (!won ) {
+                JOptionPane.showMessageDialog(null,"You lose. The number was: " + game.getNumber());
             }
-            System.out.print("Play again? (yes/no): ");
-            String again = sc.next();
+            String again = JOptionPane.showInputDialog("Play again? (yes/no): ");
             if (!again.equalsIgnoreCase("yes")) {
                 break;
             }
